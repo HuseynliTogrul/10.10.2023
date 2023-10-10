eb4fc3073b2dd77f523433af5044cdae4816b16c
HuseynliTogrul <toghrulih@code.edu.az>
2023-10-11 01:17:27 +0400

    Substring

---
+++ Substring/Substring/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+string str = "hello world";
+string newstr = "";
+
+Console.WriteLine(Substring(str, 0, 2));
+string Substring(string str, int j, int length)
+{
+    for (int i = j; i <= length; i++)
+{
+    newstr += str[i];
+}   return newstr;
+}
+
+
